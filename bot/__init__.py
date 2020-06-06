7fea65fe2ef887c72a79cde580967ec318c95754
makubob <makupi@users.noreply.github.com>
2020-06-06 03:36:36 +0200

    Version Bump v0.1.1

--- bot/__init__.py
+++ bot/__init__.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 from bot.utils import config
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 invite_link = "https://discordapp.com/api/oauth2/authorize?client_id={}&scope=bot&permissions=8192"
 
