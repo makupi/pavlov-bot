c3a87356e7e9b4ab0ea7c3026b25fe6ad9707521
Derpyhsi <wacktru3steam@hotmail.com>
2021-10-10 02:10:18 -0500

    Update pavlov_mod.py

--- bot/cogs/pavlov_mod.py
+++ bot/cogs/pavlov_mod.py
@@ -117,6 +117,114 @@ class PavlovMod(commands.Cog):
             )
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def addmod(
+        self, ctx, player_arg: str, server_name: str = config.default_server
+    ):
+        """`{prefix}addmod <player_id> <server_name>`
+
+        **Requires**: Moderator permissions or higher for the server
+        **Example**: `{prefix}addmod 89374583439127 rush`
+        """
+        if not await check_perm_moderator(ctx, server_name):
+            return
+        player = SteamPlayer.convert(player_arg)
+        data = await exec_server_command(ctx, server_name, f"AddMod {player.unique_id}")
+        amod = data.get("AddMod")
+        if ctx.batch_exec:
+            return amod
+        if not amod:
+            embed = discord.Embed(
+                description=f"**Failed** to add <{player.unique_id}> to mods.txt"
+            )
+        else:
+            embed = discord.Embed(
+                description=f"<{player.unique_id}> successfully added to mods.txt"
+            )
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def removemod(
+        self, ctx, player_arg: str, server_name: str = config.default_server
+    ):
+        """`{prefix}removemod <player_id> <server_name>`
+
+        **Requires**: Moderator permissions or higher for the server
+        **Example**: `{prefix}removemod 89374583439127 rush`
+        """
+        if not await check_perm_moderator(ctx, server_name):
+            return
+        player = SteamPlayer.convert(player_arg)
+        data = await exec_server_command(ctx, server_name, f"RemoveMod {player.unique_id}")
+        rmod = data.get("RemoveMod")
+        if ctx.batch_exec:
+            return rmod
+        if not rmod:
+            embed = discord.Embed(
+                description=f"**Failed** to remove <{player.unique_id}> from mods.txt"
+            )
+        else:
+            embed = discord.Embed(
+                description=f"<{player.unique_id}> successfully removed from mods.txt"
+            )
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def slap(
+        self, ctx, player_arg: str, dmg: str, server_name: str = config.default_server
+    ):
+        """`{prefix}slap <player_id> <damage_amount> <server_name>`
+
+        **Requires**: Moderator permissions or higher for the server
+        **Example**: `{prefix}slap 89374583439127 10 rush`
+        """
+        if not await check_perm_moderator(ctx, server_name):
+            return
+        player = SteamPlayer.convert(player_arg)
+        data = await exec_server_command(ctx, server_name, f"Slap {player.unique_id} {dmg}")
+        slapd = data.get("Successful")
+        if ctx.batch_exec:
+            return slapd
+        if not slapd:
+            embed = discord.Embed(
+                description=f"**Failed** to slap <{player.unique_id}> for {dmg} hp"
+            )
+        else:
+            embed = discord.Embed(
+                description=f"<{player.unique_id}> successfully slapped for {dmg} hp"
+            )
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def setpin(
+        self, ctx, pin = '', server_name: str = config.default_server
+    ):
+        """`{prefix}setpin <pin> <server_name>`
+
+        **Requires**: Moderator permissions or higher for the server
+        **Example**: `{prefix}setpin 0000 rush`
+        """
+        if not await check_perm_moderator(ctx, server_name):
+            return
+        data = await exec_server_command(ctx, server_name, f"SetPin {pin}")
+        spin = data.get("Successful")
+        if ctx.batch_exec:
+            return spin
+        if not spin:
+            embed = discord.Embed(
+                description=f"**Failed** to set pin {pin}"
+            )
+        else:
+            if pin == '':
+                embed = discord.Embed(
+                description=f"Pin removed"
+                )
+            else:
+                embed = discord.Embed(
+                description=f"Pin {pin} successfully set"
+                )
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(PavlovMod(bot))
