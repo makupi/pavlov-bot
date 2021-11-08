0241faf27aad6f7c6970f5293e296efed9b5d257
Derpyhsi <wacktru3steam@hotmail.com>
2021-11-08 13:38:53 -0600

    Changed pollings to poll_config to make it easier to read

--- bot/cogs/polling.py
+++ bot/cogs/polling.py
@@ -20,59 +20,59 @@ class Polling(commands.Cog):
         logging.info(f"{type(self).__name__} Cog ready.")
         polls = polling.get_names()
         for poll in polls:
-            pollings = polling.get(poll)
-            servers = pollings.get("servers")
+            poll_config = polling.get(poll)
+            servers = poll_config.get("servers")
             for server in servers:
                 logging.info(f"Task {poll} created for server {server}")
                 asyncio.create_task(
                     self.new_poll(
-                        pollings,
+                        poll_config,
                         server,
                         poll,
                     )
                 )
 
-    async def new_poll(self, pollings, server: str, poll: str):
+    async def new_poll(self, poll_config, server: str, poll: str):
         while True:
             # try:
-            if pollings.get("type") == "player":
-                interval = float(pollings.get("polling_interval")) * 60
+            if poll_config.get("type") == "player":
+                interval = float(poll_config.get("polling_interval")) * 60
                 await asyncio.sleep(interval)
                 logging.info(f"Executing Task {poll} on server {server}")
                 try:
-                    state, ctx = await self.player_polling(ctx, pollings, server, state)
+                    state, ctx = await self.player_polling(ctx, poll_config, server, state)
                 except:
-                    state, ctx = await self.player_polling(None, pollings, server, None)
-                # if pollings.get("type") == "autobalance":
-                #    interval = float(pollings.get("polling_interval")) * 60
+                    state, ctx = await self.player_polling(None, poll_config, server, None)
+                # if poll_config.get("type") == "autobalance":
+                #    interval = float(poll_config.get("polling_interval")) * 60
                 #    await asyncio.sleep(interval)
                 #    logging.info(f"Executing Task {poll} on server {server}")
-                #    await self.autobalance_polling(pollings, server, poll)
+                #    await self.autobalance_polling(poll_config, server, poll)
             # except Exception as e:
             #    await asyncio.sleep(1)
             #    logging.info(f"Exception appeared in {poll} on server {server}! Exception: {e}")
             #    pass
 
-    async def player_polling(self, ctx, pollings, server, old_state):
-        channel = self.bot.get_channel(int(pollings.get("polling_channel")))
+    async def player_polling(self, ctx, poll_config, server, old_state):
+        channel = self.bot.get_channel(int(poll_config.get("polling_channel")))
         logging.info(f"Starting poll with state: {old_state}")
         data, ctx = await exec_server_command(ctx, server, "RefreshList", True)
         amt = len(data.get("PlayerList"))
-        p_role = "<@&" + str(pollings.get("polling_role")) + ">"
+        p_role = "<@&" + str(poll_config.get("polling_role")) + ">"
         logging.info(f"{server} has {amt} players")
-        if pollings.get("show_scoreboard"):
+        if poll_config.get("show_scoreboard"):
             scoreboardcmd = self.bot.all_commands.get("players")
             scoreboard = await scoreboardcmd(ctx, server)
         lows, meds, highs = (
-            pollings.get("low_threshold"),
-            pollings.get("medium_threshold"),
-            pollings.get("high_threshold"),
+            poll_config.get("low_threshold"),
+            poll_config.get("medium_threshold"),
+            poll_config.get("high_threshold"),
         )
         if int(highs) <= amt:
             new_state = "high"
             logging.info(f"New state is {new_state}")
             embed = discord.Embed(title=f"`{server}` has high population! {amt} players are on!")
-            if pollings.get("show_scoreboard"):
+            if poll_config.get("show_scoreboard"):
                 scoreboardcmd = self.bot.all_commands.get("players")
                 scoreboard = await scoreboardcmd(ctx, server)
                 embed.description = scoreboard
@@ -85,7 +85,7 @@ class Polling(commands.Cog):
             new_state = "medium"
             logging.info(f"New state is {new_state}")
             embed = discord.Embed(title=f"`{server}` has medium population! {amt} players are on!")
-            if pollings.get("show_scoreboard"):
+            if poll_config.get("show_scoreboard"):
                 scoreboardcmd = self.bot.all_commands.get("players")
                 scoreboard = await scoreboardcmd(ctx, server)
                 embed.description = scoreboard
@@ -98,7 +98,7 @@ class Polling(commands.Cog):
             new_state = "low"
             logging.info(f"New state is {new_state}")
             embed = discord.Embed(title=f"`{server}` has low population! {amt} players are on!")
-            if pollings.get("show_scoreboard"):
+            if poll_config.get("show_scoreboard"):
                 scoreboardcmd = self.bot.all_commands.get("players")
                 scoreboard = await scoreboardcmd(ctx, server)
                 embed.description = scoreboard
@@ -110,24 +110,24 @@ class Polling(commands.Cog):
         else:
             return None, ctx
 
-    # async def autobalance_polling(self, pollings, server, poll: str):
-    #    channel = self.bot.get_channel(int(pollings.get("polling_channel")))
+    # async def autobalance_polling(self, poll_config, server, poll: str):
+    #    channel = self.bot.get_channel(int(poll_config.get("polling_channel")))
     #    ctx = 'noctx'
     #    teamblue, teamred, kdalist, alivelist, scorelist = await get_stats(ctx, server)
     #    for k, v in scorelist.items():
     #        if v is None:
     #            score = 0
     #        score = int(v)
-    #        if score < int(pollings.get("tk_threshold")):
+    #        if score < int(poll_config.get("tk_threshold")):
     #            logging.info(f"Task {poll}: TK threshold triggered for {k}")
-    #            logging.info(f"Task {poll}: Peforming tk action {pollings.get('tk_action')}")
-    #            if pollings.get("tk_action").casefold() == "kick":
+    #            logging.info(f"Task {poll}: Peforming tk action {poll_config.get('tk_action')}")
+    #            if poll_config.get("tk_action").casefold() == "kick":
     #                await exec_server_command(ctx, server, f"Kick {k}")
     #                logging.info(f"Player {k} kicked for TK from server {server} at score {score}")
-    #            elif pollings.get("tk_action").casefold() == "ban":
+    #            elif poll_config.get("tk_action").casefold() == "ban":
     #                await exec_server_command(ctx, server, f"Ban {k}")
     #                logging.info(f"Player {k} banned for TK from server {server} at score {score}")
-    #            elif pollings.get("tk_action").casefold() == "test":
+    #            elif poll_config.get("tk_action").casefold() == "test":
     #                logging.info(f"Player {k} would have been actioned for TK from server {server} at score {score}")
     #                pass
     #    logging.info(f"Starting autobalance at {len(teamblue)}/{len(teamred)}")
@@ -137,10 +137,10 @@ class Polling(commands.Cog):
     #    elif len(teamred) - 1 == len(teamblue) or len(teamred) + 1 == len(teamblue):
     #        logging.info(f"Exiting autobalance on odd number equal teams")
     #        pass
-    #    elif int(pollings.get("autobalance_min_players")) > len(teamblue) + len(teamred):
+    #    elif int(poll_config.get("autobalance_min_players")) > len(teamblue) + len(teamred):
     #        logging.info(f"Exiting autobalance on min players")
     #        pass
-    #    elif int(pollings.get("autobalance_tolerance")) < abs(len(teamblue) - len(teamred)):
+    #    elif int(poll_config.get("autobalance_tolerance")) < abs(len(teamblue) - len(teamred)):
     #        try:
     #            while True:
     #                teamblue, teamred, kdalist, alivelist, scorelist = await get_stats(ctx, server)
@@ -152,7 +152,7 @@ class Polling(commands.Cog):
     #                elif len(teamblue) > len(teamred):
     #                    switcher = random.choice(teamblue)
     #                    logging.info(f"Player {switcher} moved from blue to red on {server} at playercount {len(teamblue) + len(teamred)} ratio {len(teamblue)}/{len(teamred)} ")
-    #                    if pollings.get("autobalance_testing"):
+    #                    if poll_config.get("autobalance_testing"):
     #                        logging.info(f"Just testing")
     #                        pass
     #                    else:
@@ -160,7 +160,7 @@ class Polling(commands.Cog):
     #                elif len(teamred) > len(teamblue):
     #                    switcher = random.choice(teamred)
     #                    logging.info(f"Player {switcher} moved from red to blue on {server} at playercount {len(teamblue) + len(teamred)} ratio {len(teamblue)}/{len(teamred)} ")
-    #                    if pollings.get("autobalance_testing"):
+    #                    if poll_config.get("autobalance_testing"):
     #                        logging.info(f"Just testing")
     #                        pass
     #                    else:
