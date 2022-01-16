73981955681a92939fecd6ba88d4d1d2c76f7eb1
Markus Pischinger <makupi@icloud.com>
2022-01-16 18:44:57 -0500

    Refactor Lists helper class

--- bot/utils/lists.py
+++ bot/utils/lists.py
@@ -3,38 +3,38 @@ import os
 
 
 class ListsSettingsNotFoundError(Exception):
-    def __init__(self, ListsSettings: str):
-        self.Listssettings = ""
+    def __init__(self, list_settings: str):
+        self.list_settings = list_settings
 
 
 class Lists:
     def __init__(self, filename="lists.json"):
         self._filename = filename
-        self._Listssettings = {}
+        self.__list_settings = {}
         self.ListsSettingsNotFoundError = ListsSettingsNotFoundError
         if not os.path.isfile(filename):
             with open(filename, "w") as file:
                 json.dump("{}", file)
         with open(filename) as file:
-            self._Listssettings = json.load(file)
+            self.__list_settings = json.load(file)
 
     def get(self, name: str):
-        Listssettings = self._Listssettings.get(name)
-        if Listssettings is None:
-            for key in self._Listssettings.keys():
+        list_settings = self.__list_settings.get(name)
+        if list_settings is None:
+            for key in self.__list_settings.keys():
                 if key.lower() == name.lower():
-                    server = self._Listssettings.get(key)
+                    list_settings = self.__list_settings.get(key)
                     break
             else:
                 raise ListsSettingsNotFoundError(name)
-        return Listssettings
+        return list_settings
 
     def get_by_type(self, _type: str):
         result = dict()
-        for name, list_setting in self._Listssettings.items():
+        for name, list_setting in self.__list_settings.items():
             if list_setting.get("type").lower() == _type.lower():
                 result[name] = list_setting
         return result
 
     def get_names(self, poll_group: str = None):
-        return list(self._Listssettings.keys())
+        return list(self.__list_settings.keys())
